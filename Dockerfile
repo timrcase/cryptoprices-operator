713e5e203d0005d68e2fc826798e5d87999922b7
Tim Case <tim.case@cherre.com>
2021-11-16 16:22:55 -0500

    finally got it working

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM python:3.8-alpine
+
+LABEL org.opencontainers.image.source https://github.com/timrcase/cryptoprices-operator
+
 RUN apk --update add gcc build-base
-RUN pip install --no-cache-dir kopf kubernetes requests
+RUN pip install --no-cache-dir kopf requests
 ADD cryptoprices-operator.py /
 CMD kopf run /cryptoprices-operator.py
--- cryptoprices-operator.py
+++ cryptoprices-operator.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python3
 
 import kopf
-import kubernetes.config as k8s_config
-import kubernetes.client as k8s_client
 import requests
+import json
+import os
 
 def get_price(coin, currency):
     coingecko_url = 'https://api.coingecko.com/api/v3/simple/price'
-    return requests.get(f'{coingecko_url}?ids={coin}&vs_currencies={currency}').json()[coin][currency]
+    price = requests.get(f'{coingecko_url}?ids={coin}&vs_currencies={currency}').json()[coin][currency]
+    return price
+
+def send_to_slack(coin, currency):
+    price = get_price(coin, currency)
+    payload = {'text': f'{coin} is currently {price} {str(currency).upper()}'}
+    headers = {'Content-Type': 'application/json'}
+    slack_wehbook = os.environ.get('SLACK_WEBHOOK_URL')
+    requests.post(slack_wehbook, headers=headers, data=json.dumps(payload))
 
 @kopf.on.create('operators.timrcase.github.io', 'v1', 'cryptoprices')
-def on_create(namespace, spec, body, **kwargs):
+def on_create(spec, **kwargs):
     coin = spec['coin']
     currency = spec['currency']
-    print(get_price(coin, currency))
+    send_to_slack(coin, currency)
 
 @kopf.on.update('operators.timrcase.github.io', 'v1', 'cryptoprices')
-def on_update(namespace, name, spec, status, **kwargs):
+def on_update(spec, **kwargs):
     coin = spec['coin']
     currency = spec['currency']
-    print(get_price(coin, currency))
+    send_to_slack(coin, currency)
+
+@kopf.timer('operators.timrcase.github.io', 'v1', 'exchangerates', interval=3600.0)
+def update_price(spec, **kwargs):
+    coin = spec['coin']
+    currency = spec['currency']
+    send_to_slack(coin, currency)
